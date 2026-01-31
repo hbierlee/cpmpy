99f58c414ec747fe7547d49b2a37cb39c1d23d37
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2026-01-31 17:16:44 +0000

    Fix solver name

--- cpmpy/solvers/gurobi.py
+++ cpmpy/solvers/gurobi.py
@@ -126,7 +126,7 @@ class CPM_gurobi(SolverInterface):
         except PackageNotFoundError:
             return None
 
-    def __init__(self, cpm_model=None, subsolver=None, **kwargs):
+    def __init__(self, name="gurobi", cpm_model=None, subsolver=None, **kwargs):
         """
         Constructor of the native solver object
 
@@ -145,7 +145,7 @@ class CPM_gurobi(SolverInterface):
 
         # initialise everything else and post the constraints/objective
         # it is sufficient to implement add() and minimize/maximize() below
-        super().__init__(name="gurobi", cpm_model=cpm_model, **kwargs)
+        super().__init__(name=name, cpm_model=cpm_model, **kwargs)
 
     @property
     def native_model(self):
