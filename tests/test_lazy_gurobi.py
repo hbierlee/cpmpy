026719bfeb56d8b0c41949b7548e277ef077a972
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2026-02-04 15:15:20 +0000

    Test more envs + add alias to test case name

--- tests/test_lazy_gurobi.py
+++ tests/test_lazy_gurobi.py
@@ -11,6 +11,7 @@ import cpmpy as cp
 from cpmpy.expressions.utils import show_assignment, dom_size
 from cpmpy.solvers.lazy_gurobi import CPM_lazy_gurobi, normalize_table, Heuristic
 from cpmpy.solvers.gurobi import CPM_gurobi
+from cpmpy.tools.xcsp3.experiments import get_experiments
 
 
 def generate_table_from_example():
@@ -132,26 +133,32 @@ SEED = None
 
 
 def get_envs():
-    if True:
-        yield {
-            "solver": CPM_lazy_gurobi,
-            "solver_kwargs": {
-                "env": {
-                    "verbosity": 3,
-                    "debug": 1,
-                    "max_iterations": 500,
-                    "seed": 42,
-                    "fractional": False,
-                    "coverlift": False,
-                    "negatives": 0,
-                    # "heuristic": Heuristic.INPUT,
-                    "heuristic": Heuristic.GREEDY,
-                    "cutoff": 0,
-                }
-            },
-        }
+    yield {
+        "alias": "dev",
+        "solver": CPM_lazy_gurobi,
+        "solver_kwargs": {
+            "env": {
+                "verbosity": 3,
+                "debug": 1,
+                "max_iterations": 500,
+                "seed": 42,
+                "fractional": False,
+                "coverlift": False,
+                "negatives": 0,
+                # "heuristic": Heuristic.INPUT,
+                "heuristic": Heuristic.GREEDY,
+                "cutoff": 0,
+            }
+        },
+    }
+
+    for e in get_experiments():
+        if e["alias"] == "base_gurobi":
+            continue
+        yield e
 
     yield {
+        "alias": "base_gurobi",
         "solver": CPM_gurobi,
         "solver_kwargs": {
             "verbose": 0
@@ -443,7 +450,7 @@ class TestTables:
 
 def idfn(a):
     if isinstance(a, dict):
-        return a["solver"]
+        return a["alias"]
     else:
         return f"{a[0]}-{a[1]}"
 
