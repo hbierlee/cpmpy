2594cb78a4eabad5228517d4efac31aca3b2ca42
Henk Bierlee <bierlee.henk@gmail.com>
2026-02-09 11:38:27 +0100

    Add XCSP3 encoding to lazy Gurobi tests

--- tests/test_lazy_gurobi.py
+++ tests/test_lazy_gurobi.py
@@ -162,7 +162,7 @@ def get_envs():
             continue
         yield e
 
-    for encoding in [Encoding.DEFAULT, Encoding.GLEB]:
+    for encoding in [Encoding.DEFAULT, Encoding.GLEB, Encoding.XCSP3]:
         yield {
             "alias": f"base_gurobi-{encoding}",
             "solver": CPM_gurobi,
