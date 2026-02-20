5884f95bed9963e46ee0dc29a374225fcae0d1c6
Wout <wout.piessens@kuleuven.be>
2026-02-20 16:44:47 +0100

    Addressing small issues #3

--- cpmpy/solvers/gurobi.py
+++ cpmpy/solvers/gurobi.py
@@ -778,7 +778,7 @@ class CPM_gurobi(SolverInterface):
                 if cpm_var.is_bool():
                     cpm_var._value = solver_val >= 0.5
                 else:
-                    cpm_var._value = int(solver_val)
+                    cpm_var._value = round(solver_val)
             # set _objective_value
             if self.has_objective():
                 grb_obj_val = grb_objective.getValue()
